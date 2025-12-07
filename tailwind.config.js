36dbe81c1ba9a2b03bfc2bc2490afbc7e993d71d
lwzz1232-gif <lwzz1232@gmail.com>
2025-12-07 13:43:50 +0100

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
+ content: [
     "./index.html",
-    "./**/*.{js,ts,jsx,tsx}",
+    "./screens/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./hooks/**/*.{js,ts,jsx,tsx}",
+    "./services/**/*.{js,ts,jsx,tsx}",
+    "./utils/**/*.{js,ts,jsx,tsx}",
+    "./App.tsx",
   ],
   theme: {
     extend: {
